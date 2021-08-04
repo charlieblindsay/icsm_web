ace5d4f65f02575afc04f8084a803f23917eb46b
charlieblindsay <charlieblindsay@gmail.com>
2021-08-04 12:41:00 +0200

    hello

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const html = (
   <header className='header'>
     <div className='header__text-box'>
     <h1 className="primary-title color-blue">Python Course</h1>
-    <h1 className="primary-title color-blue margin-big ">
+    <h1 className="secondary-title color-blue margin-big hidden ">
       Autumn Term
     </h1>
 <a href='#' className='btn btn-primary'>SIGN UP</a>
@@ -39,41 +39,21 @@ const html = (
         <h2 className='secondary-title'>10 Week Course</h2>
         <h3 className='tertiary-title'>Build you knowledge from the basics</h3>
         <ul>
-          <li>Interactive exercises</li>
-          <li>Real world projects</li>
-          <li>Competitions</li>
-          <li>Weekly drop-in Q&A sessions</li>
+          <li className="overview__item">Interactive exercises</li>
+          <li className="overview__item">Real world projects</li>
+          <li className="overview__item">Competitions</li>
+          <li className="overview__item">Weekly drop-in Q&A sessions</li>
         </ul>
     </div>
   </div>
 
-  <div className='structure'>
-    <ul className='structure__course-buttons'>
-    <h2 className="secondary-title margin-small">Course structure 2021</h2>
-      <li><a href="" className="btn btn-course">Section 1</a></li>
-      <li><a href="" className="btn btn-course">Section 1</a></li>
-      <li><a href="" className="btn btn-course">Section 1</a></li>
-      <li><a href="" className="btn btn-course">Section 1</a></li>
-      <li><a href="" className="btn btn-course">Section 1</a></li>
-    </ul>
-  </div>
-
-  <div className="signup">
-    <div className="row">
-      <div className="row-item">
-      <h1 className="primary-title margin-medium">All abilities are welcome</h1>
-    <a href="" className="btn btn-primary">Sign up</a>
-      </div>
-      <div className="row-item">
-      </div>
-    </div>
-  </div>
-
   <footer className="footer">
-    <img src={Logo} alt="" width="30%"/>
-    <hr></hr>
-    <ul className="row">
-      <li><a href="">hello</a><a href="">hello</a><a href="">hello</a></li>
+    <img src={Logo} alt="" height="60%"/>
+    <hr className='hr-footer'></hr>
+    <ul className="footer__list">
+      <li className="footer__item"><a href="" className="footer__link">icsm coding</a></li>
+      <li className="footer__item"><a href="" className="footer__link">contact</a></li>
+      <li className="footer__item"><a href="" className="footer__link">team</a></li>
     </ul>
   </footer>
 
