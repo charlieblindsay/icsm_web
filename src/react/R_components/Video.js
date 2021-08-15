b6fdbf7f857d5220698ddab67e34167926318854
charlieblindsay <charlieblindsay@gmail.com>
2021-08-15 18:15:59 +0100

    added media queries for font sizes to use em, changed vh to em for different sections, added exercise box under videos

--- src/react/R_components/Video.js
+++ src/react/R_components/Video.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Video = props => {
     return (
-        <div>
+        <div className='video'>
             <h3 className="tertiary-title">{props.videoTitle}</h3>
-            {/* <div className="iframe-container">
+            <div className="iframe-container">
                 <iframe type="text/html" frameborder="0" allowfullscreen="" src={props.src} data-youtubeid="G4doTGQPRPw" data-translatedyoutubeid="G4doTGQPRPw" tabindex="0" allow="autoplay"></iframe>
-            </div> */}
+            </div>
+            <div className='video-description'>
+                <h3 className='tertiary-title'>Exercise</h3>
+            </div>
         </div>
     );
 }
