73463fe59a0e02c0aeadfef93c3ce45ff44fcb77
charlieblindsay <charlieblindsay@gmail.com>
2021-09-30 00:00:30 +0100

    added team photos with descriptions

--- src/react/layout/Footer.js
+++ src/react/layout/Footer.js
@@ -9,7 +9,7 @@ function Footer(){
                 <hr className='hr-footer'></hr>
                 <ul className="footer__list">
                     <li className="footer__item"><a href="https://www.union.ic.ac.uk/medic/coding/" className="footer__link" target="_blank">icsm coding</a></li>
-                    <li className="footer__item"><a href="mailto:icsmcoding@imperial.ac.uk?subject=Python%20Course" className="footer__link">contact</a></li>
+                    <li className="footer__item"><a href="https://www.imperialcollegeunion.org/activities/a-to-z/coding-medics" className="footer__link">Membership</a></li>
 
                     <li className="footer__item"><Link className="footer__link" to='/team'>Team</Link></li>
                 </ul>
